3d388102537ec93d972d94550188579df0871c9a
michaelwillianla12 <michaelwillian@hotmail.com>
2020-09-08 02:51:14 -0300

    add exercise in Module 3

---
+++ Module 3 - Data Preparation in Pandas/exercise3.py
@@ -0,0 +1,16 @@
+import pandas as pd
+
+
+df = pd.read_csv("https://data.heatonresearch.com/data/t81-558/datasets/series-31.csv")
+df['date'] = pd.to_datetime(df['time'], errors='coerce')
+
+# print(df.groupby('time').mean())
+
+
+first = df.set_index('date').resample('d')["value"].first().dropna(how='all').rename('starting')
+last = df.set_index('date').resample('d')["value"].last().dropna(how='all').rename('max')
+minimun = df.set_index('date').resample('d')["value"].min().dropna(how='all').rename('min')
+maximun = df.set_index('date').resample('d')["value"].max().dropna(how='all').rename('ending')
+df = pd.concat([first, maximun, minimun, last], axis=1).reset_index()
+
+print(df)
